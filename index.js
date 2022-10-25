f22ebc464ad753942c5ea8870d3d4fde6c69d2bb
mursona <mursona>
2022-10-25 09:37:17 +0600

    response send api server for field data load

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 
 const researchField = require('./data/research-field.json');
+const research = require('./data/research.json');
 
 app.get('/', (req, res) => {
     res.send('Learning api running');
@@ -13,6 +14,28 @@ app.get('/', (req, res) => {
 
 app.get('/research-field', (req, res) => {
     res.send(researchField);
+});
+
+app.get('/field/:id', (req, res) => {
+    const id = req.params.id;
+    if (id === '08') {
+        res.send(research);
+    }
+    else{
+        const field_research =  research.filter(r => r.field_id === id);
+        res.send(field_research);
+    }
+});
+
+app.get('/research', (req, res) =>{
+    res.send(research);
+});
+
+app.get('/research/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedResearch = research.find(r => r._id === id);
+    res.send(selectedResearch)
+
 });
 
 app.listen(port, () => {
